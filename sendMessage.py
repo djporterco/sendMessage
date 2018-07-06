05c19dabac2dc9e8b0d0ae189281ba3942f1b201
DJ Porter <dj@redshoe.me>
2018-07-06 13:33:19 -0700

    Update sendMessage.py

--- sendMessage.py
+++ sendMessage.py
@@ -6,7 +6,7 @@
 from twilio.rest import Client
 
 def sendmessage(body):
-        sid = '[YOUR_SID_CODE_HERE]'
+    sid = '[YOUR_SID_CODE_HERE]'
     auth_token ='[YOUR_AUTH_CODE_HERE]'
     client = Client(sid, auth_token)
     message = client.messages.create(to="[YOUR_NUMBER]",from_="[YOUR_TWILIO_NUMBER]  ",body=body)
