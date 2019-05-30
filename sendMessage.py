562e25ff3de8c8b565569486a1e387937d3664b4
DJ Porter <dj@redshoe.me>
2019-05-30 14:05:50 -0500

    Update sendMessage.py

--- sendMessage.py
+++ sendMessage.py
@@ -9,6 +9,6 @@ def sendmessage(body):
     sid = '[YOUR_SID_CODE_HERE]'
     auth_token ='[YOUR_AUTH_CODE_HERE]'
     client = Client(sid, auth_token)
-    message = client.messages.create(to="[YOUR_NUMBER]",from_="[YOUR_TWILIO_NUMBER]  ",body=body)
+    message = client.messages.create(to="[YOUR_NUMBER]",from_="[YOUR_TWILIO_NUMBER]",body=body)
 
 sendmessage('test')
